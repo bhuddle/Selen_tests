f9bede7007b30dcc947345a4a718bd4fde0bb01d
Ben <benjaminehuddle@gmail.com>
2019-01-12 19:48:40 -0800

    updated browser

--- SeleniumBrowser.py
+++ SeleniumBrowser.py
@@ -3,22 +3,18 @@ from selenium.webdriver.common.keys import Keys
 import time
 
 def Simple_Window():
-    browser = webdriver.Chrome('C:\Python27\ChromeDriver\chromedriver.exe')
+    browser = webdriver.Firefox()
     time.sleep(2)
     browser.get('http://www.swagbucks.com/p/login')
     time.sleep(5)
-    raw_input('Press enter to continue after CAPCHA & logging in')
-    time.sleep(20)
-    """ Need to get past capcha
-    browser.find_element_by_id('sbLogInCta').click()
-    time.sleep(5)
-    browser.find_element_by_id('sbxJxRegEmail').send_keys('test@email.com')
-    time.sleep(5)
-    browser.find_element_by_id('sbxJxRegPswd').send_keys('1234')
+    browser.find_element_by_id('sbxJxRegEmail').send_keys('benjaminehuddle@gmail.com')
     time.sleep(5)
     browser.find_element_by_class_name('recaptcha-anchor').click()
     time.sleep(5)
-    browser.find_element_by_id('loginBtn').click()"""
+    raw_input('Press enter to continue after CAPCHA & logging in')
+    time.sleep(5)
+    browser.find_element_by_id('loginBtn').click()
+    time.sleep(30)
     browser.find_element_by_id('sbGlobalNavSearchInputWeb').send_keys('Cats and dogs')
     time.sleep(1)
     browser.find_element_by_id('sbGlobalNavSearchInputWeb').send_keys(Keys.ENTER)

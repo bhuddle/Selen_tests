4212e2d8a5d6aed940a374300bc3b6d4b68bd493
Ben <benjaminehuddle@gmail.com>
2019-01-18 21:30:41 -0800

    check ascii, non ascii, escape characters

--- CheckSearchFunction.py
+++ CheckSearchFunction.py
@@ -10,6 +10,30 @@ def Check_Search():
     browser.get(url)
     time.sleep(5)
     
+    testText = []
+    testText.append('Normal Search')
+    testText.append('1/19/2019')
+    testText.append('&#160;')
+    testText.append('&#160')
+    testText.append(u'\xe8 \xe9 \xe10 \xc3 \xc4 \xc5')
+    testText+='^\ \\ \\\\ ^^'.split()
+    print testText
+    longword = ''
+    for a in range(0,2000):
+        longword = longword + 'W'
+    testText.append(longword)
+    
+    for test in testText:
+        searchBar = browser.find_element_by_id('s')
+        searchButton = browser.find_element_by_id('searchsubmit')
+        searchBar.clear()
+        searchBar.send_keys(test)
+        searchButton.click()
+        time.sleep(5)
+        browser.get(url)
+        time.sleep(5)
+        
+    
     """
     Go to website
     Enter in Ascii characters

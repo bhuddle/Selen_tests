c63f7efc1dc37b9ac7b96a6d645e4576a523c47b
Ben <benjaminehuddle@gmail.com>
2019-01-22 21:18:31 -0800

    modified file to be unit test

--- ClickAllLinks.py
+++ ClickAllLinks.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 import time
 
-def Check_Links():
+def test_Check_Links():
     url = 'http://www.powinenergy.com/news'
     browser = webdriver.Chrome('C:\Python27\ChromeDriver\chromedriver.exe')
     browser.maximize_window()
@@ -13,24 +13,20 @@ def Check_Links():
     print "Number of links: "+str(len(links)) 
     for link in range(len(links)):
         link2 = str(links[link].get_attribute('href'))
-        if '#' in link2 or '@' in link2 or 'tel' in link2:
-            print "Skipping:  "+str(links[link].get_attribute('href'))       
+        if '#' in link2 or '@' in link2 or 'tel' in link2 or 'linkedin' in link2:
+            print "Skipping:  "+link2
         else:
-            print "Clicking: "+str(links[link].get_attribute('href'))
+            print "Clicking: "+link2
             links[link].click()
+            time.sleep(5)
+            assert link2 in browser.current_url
             time.sleep(5)
             browser.get(url)
             time.sleep(5)
             links = browser.find_elements_by_xpath('//a[@href]')
     print "all links clicked"
     time.sleep(5)
-    browser.quit()
-
-Check_Links()
-
-
+    browser.quit()  
 """
-for checking with assertion:
-url = browser.get_current_url()
-assert url in "url.com/to/test"
+when it gets to linked in it doesnt work.
 """
